80b4e604630073a0be94ae2eb49d2402386c03b5
mggbhn <maggiebswim@gmail.com>
2019-01-23 18:54:48 +0000

    Automatically backed up by Learn

--- macarena.rb
+++ macarena.rb
@@ -9,11 +9,11 @@ puts ""
 puts "Step 1: Right arm out in front of you, palm facing down."
 puts "Step 2: Left arm out in front of you, palm facing down."
 # Step 2: FIXME
-puts "Step 2: Turn your right palm up."
+puts "Step 3: Turn your right palm up."
 puts "Step 4: Turn your left palm up."
 # Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
-
+puts "Step 5: Place your right hand on your left shoulder."
+puts "Step 6: Place your left hand on your right shoulder."
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
 
